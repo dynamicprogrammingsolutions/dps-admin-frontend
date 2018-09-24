39a2760a88fdb6553d17b3c87dc408631f5694a6
Zoltan Laszlo Ferenci <ferenci84@gmail.com>
2018-09-24 14:47:58 +0200

    gulp & npm tasks

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ const postcssclean = require('postcss-clean');
 const postcssdiscarddublicates = require('postcss-discard-duplicates');
 const cssimport = require('postcss-import');
 const sass = require('gulp-sass');
+const less = require('gulp-less');
+const runSequence = require('run-sequence');
 
 var sass_include_paths = ['node_modules/foundation-sites/scss'];
 
@@ -19,28 +21,11 @@ gulp.task('postcss', function() {
     return postcss_pipe(gulp.src(['styles-dev/*.css']));
 });
 
-function scss() {
-    return gulp.src('styles-dev/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({
-            includePaths: sass_include_paths
-        }))
-        .pipe(sourcemaps.write("."))
-        .pipe(gulp.dest("styles-dev"));
-}
 
-gulp.task('scss', function() {
-    return scss();
+gulp.task('css', function(cb) {
+    runSequence(['scss','less'],'postcss',cb);
 })
 
-gulp.task('watch_scss', ['scss'], function() {
-    return watch(['styles-dev/**/*.scss'])
-        .on("data",function(file) {
-            var date = new Date();
-            console.log('watch_scss',date.toLocaleString(),file.history[0].substr(file.cwd.length));
-            gulp.start('scss');
-        })
-});
 
 gulp.task('watch_css', ['postcss'], function() {
     return postcss_pipe(watch('styles-dev/*.css')
@@ -50,4 +35,79 @@ gulp.task('watch_css', ['postcss'], function() {
         })
     );
 });
+
+//reusable less pipe
+function less_pipe(pipe) {
+    return pipe
+        .pipe(sourcemaps.init())
+        .pipe(less())
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("styles-dev"));
+}
+
+// Recompiling all less files
+gulp.task('less', function() {
+    return less_pipe(gulp.src('styles-dev/*.less'));
+})
+
+// If you are making changes in the root files, only those files will be recompiled
+gulp.task('watch_less_root', ['less'], function() {
+    return less_pipe(watch('styles-dev/*.less')
+        .on("data",function(file) {
+            var date = new Date();
+            console.log('watch_less',date.toLocaleString(),file.history[0].substr(file.cwd.length));
+        })
+    );
+});
+
+// If you are making changes in the subdirectories, all the root files under styles-dev will be recompiled
+gulp.task('watch_less_inc', function() {
+    return watch(['styles-dev/*/**/*.less'])
+        .on("data",function(file) {
+            var date = new Date();
+            console.log('watch_less_inc',date.toLocaleString(),file.history[0].substr(file.cwd.length));
+            gulp.start('less');
+        })
+});
+
+gulp.task('watch_less', function(cb) {
+    runSequence(['watch_less_root','watch_less_inc'],cb);
+})
+
+// Similar structure than for less
+function scsspipe(pipe) {
+    return pipe
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: sass_include_paths
+        }))
+        .pipe(sourcemaps.write("."))
+        .pipe(gulp.dest("styles-dev"));
+}
+
+gulp.task('scss', function() {
+    return scsspipe(gulp.src('styles-dev/*.scss'));
+})
+
+gulp.task('watch_scss_root', ['scss'], function() {
+    return less_pipe(watch('styles-dev/*.less')
+        .on("data",function(file) {
+            var date = new Date();
+            console.log('watch_less',date.toLocaleString(),file.history[0].substr(file.cwd.length));
+        })
+    );
+});
+
+gulp.task('watch_scss_inc', function() {
+    return watch(['styles-dev/*/**/*.scss'])
+        .on("data",function(file) {
+            var date = new Date();
+            console.log('watch_scss',date.toLocaleString(),file.history[0].substr(file.cwd.length));
+            gulp.start('scss');
+        })
+});
+
+gulp.task('watch_scss', function(cb) {
+    runSequence(['watch_scss_root','watch_scss_inc'],cb);
+})
 
